103e896d7073f5dc3d000215a0335eac7f0ccad5
msanlub898 <msanlub898@g.educaand.es>
2023-10-30 09:58:28 +0100

    hasta el 2

---
+++ src/ejercicio1.py
@@ -0,0 +1,14 @@
+'''Escribe un bucle while que comience con el último carácter en la cadena y haga un recorrido hacia atrás hasta el primer carácter en la cadena, imprimiendo cada letra en una línea independiente.'''
+
+#entrada
+hola = "Hola Mundo"
+cadenas = []
+#proceso
+def recorrido(hola:str) -> list:
+    while cadenas < len(hola):
+        cadenas.append = hola[::-1]
+
+#salida
+print(cadenas)
+
+
---
+++ src/ejercicio2.py
@@ -0,0 +1,14 @@
+#Dado que fruta es una variable de tipo cadena, ¿qué significa fruta[:]?
+
+
+fruta = "fruta"
+print(fruta[:])
+'''imprime todos los caracteres de fruta (fruta)'''
+print(fruta[:1])
+'''imprime el segundo caracter (r)'''
+print(fruta[:-1])
+'''imprime todo menos el último caracter(frut)'''
+print(fruta[::-1])
+'''imprime fruta inverso(aturf)'''
+print(fruta[::-3])
+'''imprime el último carácter y salta dos hasta el tecero (ar)'''
